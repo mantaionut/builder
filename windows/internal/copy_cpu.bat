e0b2c42bfa813b0db2bd6d5f1db0a5e66ed96601
mantaionut <ionut@janeasystems.com>
2024-01-24 19:22:07 +0200

    Updated MKL on Windows to 2022.2.1

--- windows/internal/copy_cpu.bat
+++ windows/internal/copy_cpu.bat
@@ -3,6 +3,6 @@ copy "%CONDA_LIB_PATH%\libiomp*5md.dll" pytorch\torch\lib
 copy "%libuv_ROOT%\bin\uv.dll" pytorch\torch\lib
 
 IF "%PACKAGE_TYPE%"=="libtorch" (
-    copy "%CONDA_LIB_PATH%\mkl_intel_thread.1.dll" pytorch\torch\lib
-    copy "%CONDA_LIB_PATH%\mkl_core.1.dll" pytorch\torch\lib
+    copy "%CONDA_LIB_PATH%\mkl_intel_thread.2.dll" pytorch\torch\lib
+    copy "%CONDA_LIB_PATH%\mkl_core.2.dll" pytorch\torch\lib
 )
